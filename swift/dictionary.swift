c59a4102762fd43519eb88083c2c90e36fb87dbd
Apple <apple@Apples-iMac.local>
2019-04-01 18:21:58 +0530

    i am liking swift and its explanation by apple.

---
+++ swift/dictionary.swift
@@ -0,0 +1,4 @@
+#!/usr/bin/swift
+var dict = Dictionary<String, Float>();
+dict["rohit"] = 4.5;
+print(dict);
---
+++ swift/vvariables.swift
@@ -0,0 +1,5 @@
+#!/usr/bin/swift
+let apples = 5;
+let oranges = 6;
+let appleSummary = "I have \(apples) apples";
+print(appleSummary);
