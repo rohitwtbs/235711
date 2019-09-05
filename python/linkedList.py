edac96e9a45418df111f2bb63fc57c96cf328b58
rohitwtbs <12rohit4@gmail.com>
2019-09-06 02:15:18 +0530

    bad code - tried reversing linkedlist
    got stuck in how objects are being called in python.

--- python/linkedList.py
+++ python/linkedList.py
@@ -4,13 +4,13 @@ class Node:
 
 	def __init__(self, data):
 		self.data = data
-		self.next = None 
+		self.next = None
 
 
 
 class LinkedList:
 
-	
+
 	def __init__(self):
 		self.head = None
 
@@ -18,17 +18,18 @@ class LinkedList:
 		print "printing linkedlist"
 		temp = self.head
 		while (temp):
-			
+
 			print temp.data,
 			temp = temp.next
-	# def reverseList(self, temp):
-	# 	print "printing reverseList"
-	# 	temp = self.head
-	# 	while (temp):
-	# 		temp = temp.next
-	# 		self.reverseList(temp)
-	# 		print temp.data
 
+	def reverseList(this):
+		print "printing reverseList"
+		temp = this.head
+		if temp.next:
+			print temp.data
+			temp = temp.next
+			this.reverseList(temp)
+			print temp.data
 	def addNode(self, node):
 		temp = self.head
 		while (temp.next):
@@ -50,9 +51,9 @@ if __name__ == '__main__':
 		else:
 			# print "in else part"
 			llist.head = Node(element)
-			
+
 		count = count - 1
-	
+
 
 	llist.printList()
-	# llist.reverseList()
+	llist.reverseList()
