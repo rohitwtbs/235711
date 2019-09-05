7d3f631cde5ed5b3316ec40eb703254db0c543ef
rohitwtbs <12rohit4@gmail.com>
2019-09-05 19:48:50 +0530

    first linkelist in python.. need to implement more

---
+++ python/linkedList.py
@@ -0,0 +1,58 @@
+
+class Node:
+
+
+	def __init__(self, data):
+		self.data = data
+		self.next = None 
+
+
+
+class LinkedList:
+
+	
+	def __init__(self):
+		self.head = None
+
+	def printList(self):
+		print "printing linkedlist"
+		temp = self.head
+		while (temp):
+			
+			print temp.data,
+			temp = temp.next
+	# def reverseList(self, temp):
+	# 	print "printing reverseList"
+	# 	temp = self.head
+	# 	while (temp):
+	# 		temp = temp.next
+	# 		self.reverseList(temp)
+	# 		print temp.data
+
+	def addNode(self, node):
+		temp = self.head
+		while (temp.next):
+			temp = temp.next
+		temp.next = node
+
+
+# Code execution starts here
+if __name__ == '__main__':
+
+
+	llist = LinkedList()
+	count = input()
+	while (count):
+		element = input()
+		if llist.head:
+			# print "in if part"
+			llist.addNode(Node(element))
+		else:
+			# print "in else part"
+			llist.head = Node(element)
+			
+		count = count - 1
+	
+
+	llist.printList()
+	# llist.reverseList()
