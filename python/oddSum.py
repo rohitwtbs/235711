d92bd8455b0a5d9684d18f2b9814f26ace95efad
rohitwtbs <12rohit4@gmail.com>
2019-09-25 01:48:44 +0530

    oddSum

---
+++ python/oddSum.py
@@ -0,0 +1,26 @@
+# odd sum
+# determine the sum of all positive integers till n
+def oddSum(n):
+    noOfTermInSeries = noOfTerms(n)
+    sum = sumOfTerms(noOfTermInSeries)
+    print sum
+
+
+
+def noOfTerms(n):
+    if n&1 == 0:
+        num = n-1
+    else:
+        num = n
+    noTerm = (num + 1) /2
+    return noTerm
+
+def sumOfTerms(noOfTerm):
+    sum = pow(noOfTerm,2)
+    return sum
+
+count = input()
+while(count):
+    number = input()
+    oddSum(number)
+    count = count - 1
