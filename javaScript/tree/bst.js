dba9d25dca17058b2cb3de0203818310eaa8a64e
rohitwtbs <12rohit4@gmail.com>
2020-02-16 02:23:48 +0530

    added delete a node
    - still need more thorough understanding on this
    - having soem bugs
    - need to solve hackerrank problems now
    -donot why they donot have javascript for solving tree questions

--- javaScript/tree/bst.js
+++ javaScript/tree/bst.js
@@ -13,9 +13,9 @@ class BST {
         this.root = null;
     }
 
-    getRootNode() { 
-    return this.root; 
-    } 
+    getRootNode() {
+        return this.root;
+    }
 
     insert(data) {
         let node = new Node(data);
@@ -28,14 +28,14 @@ class BST {
     }
 
     insertNode(node, newNode) {
-        if(node.data > newNode.data) {
-            if(node.left === null) {
+        if (node.data > newNode.data) {
+            if (node.left === null) {
                 node.left = newNode;
             } else {
                 this.insertNode(node.left, newNode);
             }
         } else {
-            if(node.right === null) {
+            if (node.right === null) {
                 node.right = newNode;
             } else {
                 this.insertNode(node.right, newNode);
@@ -69,39 +69,96 @@ class BST {
     }
 
     findMinNode(node) {
-        if ( node.left === null) {
+        if (node.left === null) {
             console.log(node.data);
-            return node.data;
+            return node;
         }
         this.findMinNode(node.left);
     }
-    
+
     findMaxNode(node) {
         if (node.right == null) {
             console.log(node.data);
-            return node.data;
+            return node;
         }
         this.findMaxNode(node.right);
     }
+
+    delete(data) {
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, key) {
+        // if the root is null then tree is  
+        // empty 
+        if (node === null)
+            return null;
+
+        // if data to be delete is less than  
+        // roots data then move to left subtree 
+        else if (key < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        }
+
+        // if data to be delete is greater than  
+        // roots data then move to right subtree 
+        else if (key > node.data) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        }
+
+        // if data is similar to the root's data  
+        // then delete this node 
+        else {
+            // deleting node with no children 
+            if (node.left === null && node.right === null) {
+                node = null;
+                return node;
+            }
+
+            // deleting node with one children 
+            if (node.left === null) {
+                node = node.right;
+                return node;
+            }
+
+            else if (node.right === null) {
+                node = node.left;
+                return node;
+            }
+
+            // Deleting node with two children 
+            // minumum node of the rigt subtree 
+            // is stored in aux 
+            var aux = this.findMinNode(node.right);
+            node.data = aux.data;
+
+            node.right = this.removeNode(node.right, aux.data);
+            return node;
+        }
+    }
 }
 
-var bst = new BST(); 
-   
-bst.insert(15); 
-bst.insert(25); 
-bst.insert(10); 
-bst.insert(7); 
-bst.insert(22); 
-bst.insert(17); 
-bst.insert(13); 
-bst.insert(5); 
-bst.insert(9); 
+var bst = new BST();
+
+bst.insert(15);
+bst.insert(25);
+bst.insert(10);
+bst.insert(7);
+bst.insert(22);
+bst.insert(17);
+bst.insert(13);
+bst.insert(5);
+bst.insert(9);
 bst.insert(27);
 
-// bst.inorder(bst.getRootNode());
-// console.log('wait');
+bst.inorder(bst.getRootNode());
+console.log('wait');
 // bst.preorder(bst.getRootNode());
 // console.log('wait');
 // bst.postorder(bst.getRootNode());
-bst.findMinNode(bst.getRootNode());
-bst.findMaxNode(bst.getRootNode());
+// bst.findMinNode(bst.getRootNode());
+// bst.findMaxNode(bst.getRootNode());
+bst.delete(10);
+bst.inorder(bst.getRootNode());
