5f119e75c6b8947942bba76a2475a94da74fb603
rohitwtbs <12rohit4@gmail.com>
2020-06-29 14:31:14 +0530

    void return check

---
+++ java/voidCheck.java
@@ -0,0 +1,11 @@
+public class voidCheck {
+    
+    public static void main(String[] args) {
+        voidCheck  v = new voidCheck();
+        v.check();
+    }
+    public void check() {
+        System.out.println("check");
+        int a = 5;
+    }
+}
