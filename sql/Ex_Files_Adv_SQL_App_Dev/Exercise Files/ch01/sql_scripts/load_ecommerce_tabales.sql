64a668c16db1bc712b661e086076e56a2443beee
rohitwtbs <12rohit4@gmail.com>
2021-08-15 00:11:44 +0530

    SQL practice session.
    sql practice session

---
+++ sql/Ex_Files_Adv_SQL_App_Dev/Exercise Files/ch01/sql_scripts/load_ecommerce_tabales.sql
@@ -0,0 +1,4 @@
+COPY product_orders FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/product_orders.csv' DELIMITER ',' CSV HEADER;
+COPY products FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/products.csv' DELIMITER ',' CSV HEADER;
+COPY orders FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/orders.csv' DELIMITER ',' CSV HEADER;
+COPY customers FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/customers.csv' DELIMITER ',' CSV HEADER;
