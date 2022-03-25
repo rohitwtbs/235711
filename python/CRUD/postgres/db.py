bd74b3dcaa49b6472e792c1af36c0b81abce9520
EC2 Default User <ec2-user@ip-172-31-3-207.ap-south-1.compute.internal>
2022-03-25 12:47:18 +0000

    commiting all the changes

---
+++ python/CRUD/postgres/db.py
@@ -0,0 +1,4 @@
+# write crud operations 
+# for postgresql using ORM
+# and also write without orm
+
---
+++ python/list_comprehension.py
@@ -0,0 +1,15 @@
+
+def generate_permutation(x,y,z,n):
+    print(x,y,z,n)
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    print(x,y,z,n)
+    generate_permutation(x,y,z,n)
+
+
+def generate_permutation(x,y,z,n):
+    print(x,y,z,n)
+    print [count in x]
