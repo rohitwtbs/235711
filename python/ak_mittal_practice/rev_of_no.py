f94e73162a9acae12d947aa03e335b585a7a0353
rohitwtbs_comfy <rohit.kumar@comfyapp.com>
2022-05-10 17:57:41 +0530

    push the fucking minimal practice that you do

---
+++ python/ak_mittal_practice/rev_of_no.py
@@ -0,0 +1,5 @@
+num = str(input())
+print(str.rev())
+length = len(num)
+for i=length in range (length, 0):
+    print(num[i])
---
+++ python/ak_mittal_practice/sum_of_digits.py
@@ -0,0 +1,6 @@
+
+num =str(input())
+sum = 0
+for i in num:
+    sum = sum + int(i)
+print(sum)
