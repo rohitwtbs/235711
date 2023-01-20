b456527dc20fe3ac7721092effa3ecb9cfa3a91b
rohitwtbs <12rohit4@gmail.com>
2023-01-21 01:43:51 +0530

    type check of an object with different types

---
+++ python/type_check.py
@@ -0,0 +1,23 @@
+class Flower():
+    def __init__(self):
+        return
+    def smell(self):
+        return
+
+class Lillies(Flower):
+    def __init__(self):
+        return
+    def color():
+        return
+
+
+objA = Flower()
+
+objB = Lillies()
+
+
+print(type(objA) == Flower)
+
+print(type(objB) == Flower)
+
+print(isinstance(objB, Flower))
