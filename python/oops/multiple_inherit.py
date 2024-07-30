30df673fbb418effa5d03fd7d80484baebc3fa47
rohit kumar <rohit.kumar@comfyapp.com>
2024-07-30 19:17:25 +0530

    multiple inheritance example

--- python/oops/multiple_inherit.py
+++ python/oops/multiple_inherit.py
@@ -3,3 +3,33 @@
 class A:
     def __init__(self):
         return
+    def check(self):
+        a = 10
+        print(a)
+class B:
+    def __init__(self):
+        pass
+    def check(self):
+        b=11
+        print(b)
+    
+# class C(A,B):
+#     def __init__(self):
+#         pass
+#     def call(self):
+#         print("call called")
+
+
+
+class C(B,A):
+    def __init__(self):
+        pass
+    def call(self):
+        print("call called")
+    def check(self):
+        print("the call before the super")
+        super().check()
+
+obj_c = C()
+obj_c.call()
+obj_c.check()
